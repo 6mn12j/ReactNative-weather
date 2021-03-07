36e0fb93f4cd23a877440dbbd54e37452081aa23
6mn12j <dbwk0404@hanmail.net>
2021-03-07 22:41:58 +0900

    start react-native

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello!!!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
@@ -17,5 +19,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
   },
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+# ReactNative-weather
+Learning React Native By Building a Weather App
