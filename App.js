a8acbd04658e10fb04fe9ae48d5a47a7e9d73b13
6mn12j <dbwk0404@hanmail.net>
2021-03-08 02:35:58 +0900

    Feat: Getting the Weather

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Alert } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <>
-      <Loading />
-    </>
-  );
+const API_KEY = 'dca781ef6fedf311b9d73fb9e3efa2f5';
+
+export default class extends React.Component {
+  state = { isLoading: true };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  };
+
+  getLocation = async () => {
+    try {
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+
+      //Send to APO and get Weather
+    } catch (error) {
+      Alert.alert("Can't find you.", 'So sad');
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
