ab9b96834c16ea7550b7c4392ebc56729a8eb06d
6mn12j <dbwk0404@hanmail.net>
2021-03-08 00:59:15 +0900

    Feat: add Loading.js

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello!!!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Loading />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-  },
-});
